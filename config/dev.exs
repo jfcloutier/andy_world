e1c11335006105f22bd8bac78710ca39f6e44b76
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2022-06-22 16:43:20 -0400

    fixes

--- config/dev.exs
+++ config/dev.exs
@@ -12,13 +12,13 @@ config :andy_world, AndyWorldWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
+    # node: [
+    #   "node_modules/webpack/bin/webpack.js",
+    #   "--mode",
+    #   "development",
+    #   "--watch-stdin",
+    #   cd: Path.expand("../assets", __DIR__)
+    # ]
   ]
 
 # ## SSL Support
